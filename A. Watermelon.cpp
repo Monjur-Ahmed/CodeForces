9207a14c64777b05eeb81b20fb8c0d13f5a44be0
Monjur-Ahmed <munjorahmed@gmail.com>
2021-12-17 22:40:49 +0600

    first commit

---
+++ A. Watermelon.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int w,r;
+
+    while(scanf("%d",&w)!=EOF)
+    {
+
+    if(w%2!=0)
+    {
+        cout<<"NO"<<endl;
+    }
+    else if(w==2)
+    {
+       cout<<"NO"<<endl;
+    }
+    else
+    {
+           cout<<"YES"<<endl;
+    }
+    }
+}
